83e6520762cea249c88e52b5995eb35b27d5a04d
madou <laheen@gmail.com>
2015-11-15 00:38:58 +1100

    Fixed userlinks hiding non authenticated links

--- src/app/components/dumb/user-links/index.js
+++ src/app/components/dumb/user-links/index.js
@@ -13,7 +13,7 @@ function component () {
 			username: '@'
 		},
 		template: `
-			<ul class="${styles.userLinks}" ng-if="ctrl.loggedIn">
+			<ul class="${styles.userLinks}">
 				<li ng-if="!ctrl.loggedIn"><a title="Signup" ui-sref="main.no-auth.with-container.signup">signup</a></li>
 				<li ng-if="!ctrl.loggedIn"><a title="Login" ui-sref="main.no-auth.with-container.login">login</a></li>
 				<li ng-if="ctrl.loggedIn"><a title="Me" ui-sref="main.with-auth.me">{{ ctrl.username }}</i></a></li>
