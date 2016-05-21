4a9ad3880b9f1e6987a63d841be223725dd705be
madou <laheen@gmail.com>
2016-05-21 12:58:41 +1000

    Fixes robots.txt to correct name

--- scripts/robottxt-gen.sh
+++ scripts/robottxt-gen.sh
@@ -2,9 +2,9 @@
 
 case "$1" in
   allow)
-    printf "User-agent: *\nDisallow:" > dist/robot.txt;;
+    printf "User-agent: *\nDisallow:" > dist/robots.txt;;
   disallow)
-    printf "User-agent: *\nDisallow: /" > dist/robot.txt;;
+    printf "User-agent: *\nDisallow: /" > dist/robots.txt;;
   *)
     echo "Available tasks: {allow|disallow}"
     exit 1;;
